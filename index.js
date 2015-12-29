ec05ab2702480d882e2ce80ba8c9bce3d5db2b24
Joe Heck <heckj@mac.com>
2015-12-29 10:23:43 -0800

    straight up jQuery

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-var AppView = require('./app-view');
-var appView = new AppView();
+// Backbone components
+// var AppView = require('./app-view');
+// var appView = new AppView();
+
+// stock jQuery
+var ui = require('jquery-ui');
+global.$ = require('jquery');
+
+$("#date").datepicker();
+$("#menu").menu();
